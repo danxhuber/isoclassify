7e5bfc48461db2bc998696577c80dad3910d096e
alexlyttle <a.j.lyttle@outlook.com>
2020-05-21 12:58:12 +0100

    Added entry points and requirements

--- setup.py
+++ setup.py
@@ -4,10 +4,13 @@ from setuptools import setup
 __version__ = None
 # exec(open('isoclassify/version.py').read())
 
-# Load requirements (NB: mwdust requires manual install to download maps)
+# Load requirements
 requirements = None
-# with open('requirements.txt') as file:
-#     requirements = file.read().splitlines()
+with open('requirements.txt') as file:
+    requirements = file.read().splitlines()
+
+# mwdust requires manual install to download maps
+requirements.remove('mwdust')
 
 setup(
     name='isoclassify',
@@ -16,4 +19,9 @@ setup(
     packages=['isoclassify'],
     author='Daniel Huber',
     install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'isoclassify = isoclassify.isoclassify:main',
+        ],
+    }
 )
